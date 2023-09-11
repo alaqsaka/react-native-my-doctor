1dde689c21a93dcc93d52e8b770b3904d9ed32cd
alaqsaka <aqsaabidin15@gmail.com>
2023-09-11 16:25:14 +0700

    install react-native-svg, react-native-svg-transformer, restructure project folders

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Welcome App TSX</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
